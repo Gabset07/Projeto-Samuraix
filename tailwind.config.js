d21afa58e2dda538a2ff951141445c1f8bae7f88
Galux17 <leticia.maranhao10@gmail.com>
2023-10-25 19:15:38 -0300

    comitado

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'titulo': ['Secular One', 'sans-serif'],
+        'conteudo': ['Rubik', 'sans-serif'],}, 
+        
+        
+
+    },
+    plugins: [],
+  }
+
+
+}
